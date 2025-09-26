72bea242cd909ed19455298852e30b8a132cb439
alvarogrlp <alvarogarciaprof@gmail.com>
2025-09-26 11:48:33 +0100

    Cambios de archivos

--- Calculadora/App.tsx
+++ Calculadora/App.tsx
@@ -1,17 +1,17 @@
 import { Text, View,  StyleSheet, StatusBar, Switch } from 'react-native';
-import { ThemeContext } from './context/ThemeContext';
+import { ThemeContext } from './src/context/ThemeContext';
 import { useState } from 'react';
-import { myColors } from './styles/Colors';
+import { myColors } from './src/styles/Colors';
 
 export default function Index() {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState<'light' | 'dark'>('light');
   return (
     <ThemeContext.Provider value={theme}>
     <View style={theme === 'light' ? styles.container : [styles.container, {backgroundColor: '#000'}]}>
-      <Switch
-        value={theme === 'light'}
-        onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-      />
+    <Switch
+      value={theme === 'light'}
+      onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+    />
     </View>
     </ThemeContext.Provider>
   );
